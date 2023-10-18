e0837ca76842507aadd63832bf0305acd38f04d1
2 + 2 <ruby -v>
2023-10-17 22:06:11 -0300

    mejoras

--- scipt.js
+++ scipt.js
@@ -1,38 +1,39 @@
+var cajas1 = document.querySelector("#bar1");
+var progreso1 = document.querySelector('#nose1');
+var cajas2 = document.querySelector("#bar2");
+var progreso2 = document.querySelector('#nose2');
+var cajas3 = document.querySelector("#bar3");
+var progreso3 = document.querySelector('#nose3');
+var cajas4 = document.querySelector("#bar4");
+var progreso4 = document.querySelector('#nose4');
+var cajas5 = document.querySelector("#bar5");
+var progreso5 = document.querySelector('#nose5');
 function barras() {
-    var cajas1 = document.querySelector("#bar1");
-    var progreso1 = document.querySelector('#nose1');
     console.log('hola')
     cajas1.style.width = "100%";
     progreso1.style.visibility = "visible";
     progreso1.style.display = "block";
     progreso1.style.width = "100%";
-    var cajas2 = document.querySelector("#bar2");
-    var progreso2 = document.querySelector('#nose2');
     console.log('hola')
     cajas2.style.width = "100%";
     progreso2.style.visibility = "visible";
     progreso2.style.display = "block";
     progreso2.style.width = "100%";
-    var cajas3 = document.querySelector("#bar3");
-    var progreso3 = document.querySelector('#nose3');
     console.log('hola')
     cajas3.style.width = "100%";
     progreso3.style.visibility = "visible";
     progreso3.style.display = "block";
     progreso3.style.width = "100%";
-    var cajas4 = document.querySelector("#bar4");
-    var progreso4 = document.querySelector('#nose4');
     console.log('hola')
     cajas4.style.width = "100%";
     progreso4.style.visibility = "visible";
     progreso4.style.display = "block";
     progreso4.style.width = "100%";
-    var cajas5 = document.querySelector("#bar5");
-    var progreso5 = document.querySelector('#nose5');
     console.log('hola')
     cajas5.style.width = "100%";
     progreso5.style.visibility = "visible";
     progreso5.style.display = "block";
     progreso5.style.width = "100%";
 }
+
 
